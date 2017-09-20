bfc9c2f9fce26b984a38893fc4d35106db72779f
Mathieu Poussin <mathieu@lodgify.com>
2017-09-20 17:46:42 +0200

    Allow to pass the original version as parameter or stdin

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"github.com/blang/semver"
 	"os"
+
+	"github.com/blang/semver"
 )
 
 var (
-	action = flag.String("action", "", "Action to do: set, increment, decrement")
-	what   = flag.String("what", "patch", "Where to run the action: major, minor, patch")
-	value  = flag.Uint64("value", 1, "The value to use during the action")
+	action   = flag.String("action", "", "Action to do: set, increment, decrement")
+	what     = flag.String("what", "patch", "Where to run the action: major, minor, patch")
+	value    = flag.Uint64("value", 1, "The value to use during the action")
+	original = flag.String("original", "", "The original value, can also be omitted and passed in STDIN")
 )
 
 func updateVersionNumber(version semver.Version, action string, what string, value uint64) semver.Version {
@@ -46,10 +48,13 @@ func updateVersionNumber(version semver.Version, action string, what string, val
 func main() {
 	flag.Parse()
 	source := bufio.NewScanner(os.Stdin)
-	for source.Scan() {
-		parsedVersion, _ := semver.Make(source.Text())
+	if *original != "" {
+		parsedVersion, _ := semver.Make(*original)
 		fmt.Println(updateVersionNumber(parsedVersion, *action, *what, *value))
-
+	} else {
+		for source.Scan() {
+			parsedVersion, _ := semver.Make(source.Text())
+			fmt.Println(updateVersionNumber(parsedVersion, *action, *what, *value))
+		}
 	}
-
 }
